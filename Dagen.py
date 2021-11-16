aac9f850e527841fb619d3f1aa44a41d4424405c
Murad <99069605@mydavinci.nl>
2021-11-16 10:10:10 +0100

    zet in repo

---
+++ Dagen.py
@@ -0,0 +1,13 @@
+dagen = ['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag']
+print('Alle dagen van de week zijn:')
+print(dagen)
+print('De werkdagen zijn:')
+print(dagen[0:5])
+print('De weekenddagen zijn:')
+print(dagen[5:])
+print('De werkdagen in omgekeerde volgorde zijn:')
+dagen.reverse()
+print(dagen)
+print('De weekenddagen in omgekeerde volgorde zijn:')
+print(dagen[0:2])
+dagen.reverse()
