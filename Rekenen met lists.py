d3afad35c3a0f90a05fc380f04b6c7bcb94f97dd
Murad <99069605@mydavinci.nl>
2021-11-17 10:34:38 +0100

    rekenen met list

--- Rekenen met lists.py
+++ Rekenen met lists.py
@@ -1,9 +1,26 @@
 listOne = [1,2,3,4,5,6,7,8,9,10]
 listTwo = [2,4,6,8,10,12,14,16,18,20]
-
 def addAndDisplayLists (listOne, listTwo):
     print('--------')
     print('Add list')
     for i in range(len(listOne)):
         print(listOne[i], "+", listTwo[i], "=", listOne[i] + listTwo[i])
 addAndDisplayLists(listOne, listTwo)
+def substractAndDisplayLists (listOne, listTwo):
+    print('---------')
+    print('Substract List')
+    for i in range(len(listOne)):
+        print(listOne[i], "-", listTwo[i], "=", listOne[i] - listTwo[i])
+substractAndDisplayLists(listOne, listTwo)
+def multiplyAndDisplayLists (listOne, listTwo):
+    print('---------')
+    print('Multiply List')
+    for i in range(len(listOne)):
+        print(listOne[i], "*", listTwo[i], "=", listOne[i] * listTwo[i])
+multiplyAndDisplayLists(listOne, listTwo)
+def divideAndDisplayLists (listOne, listTwo):
+    print('---------')
+    print('Divide List')
+    for i in range(len(listOne)):
+        print(listOne[i], '/', listTwo[i], '=', listOne[i] / listTwo[i])
+divideAndDisplayLists(listOne, listTwo)
